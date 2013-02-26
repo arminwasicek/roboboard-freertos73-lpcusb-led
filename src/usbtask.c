b1d9eb8c8a604b505aee86afe3489221de4a065f
armin <armin@vmars.tuwien.ac.at>
2013-02-26 03:08:18 +0100

    Editing works.

--- src/usbtask.c
+++ src/usbtask.c
@@ -9,32 +9,61 @@
 #include "roboshell.h"
 
 #define INCREMENT_ECHO_BY 2
+#define INBUFLEN  64
 
-static int in[64];
+static int inbuf[INBUFLEN];
 
 void usbsubtask(void)
 {
 	Scanner s;
 	Token t;
 
-	int c, i=0;
+	int c, n, i=0;
+
+	s.curpos=inbuf;
 
 	// echo any character received (do USB stuff in interrupt)
 	for( ;; )
 	{
 		c = VCOM_getchar();
+
 		if (c != EOF)
 		{
-			// Echo character back with INCREMENT_ECHO_BY offset, so for example if
-			// INCREMENT_ECHO_BY is 1 and 'A' is received, 'B' will be echoed back.
-			VCOM_putchar(c + INCREMENT_ECHO_BY );
-			in[i]=c;
+			// add char to input buffer
+			if(i>=INBUFLEN)
+			{
+				i--;
+				continue ;
+			}
 
+			// echo character back
+			VCOM_putchar(c);
+
+			// check backspace
+			if((c==0x8) && (i!=0))
+			{
+				VCOM_putchar(' ');
+				VCOM_putchar('\b');
+				i--;
+				inbuf[i]='\0';
+			}
+
+			// save to input buffer
+			inbuf[i]=c;
+			i++;
+
+			//
 			if(c==0xd)               //return key
 			{
 				VCOM_putchar('\r');  //carriage return
 				VCOM_putchar(0x0a);  //line feed
+				memset(inbuf, 0, INBUFLEN);
 				i=0;
+				if( (n=scan(&s,&t))!=EOL )
+				{
+
+				}
+
 			}
 		}
 	}
